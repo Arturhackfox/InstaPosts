eb508fa23233c0fafa50d60020b839c7ca8436f8
Arthur Sh <arthur.shemshur@gmail.com>
2023-12-02 11:46:26 +0200

    - create base layer with MVVM

---
+++ Model/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  InstaPost
+//
+//  Created by Arthur Sh on 02.12.2023.
+//
+
+import Foundation
+
+
+struct Post {
+    var title: String
+    var description: String
+    var hrs: Int
+    var mins: Int
+    var hashTags: String
+}
---
+++ ViewModel/PostsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PostsViewModel.swift
+//  InstaPost
+//
+//  Created by Arthur Sh on 02.12.2023.
+//
+
+import Foundation
+
+class PostsViewModel {
+    
+}
